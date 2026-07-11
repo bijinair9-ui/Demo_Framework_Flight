8ab9d7b16dd0f7deceb5f3acbe5e6baa637c1c8c
bijinair9-ui <bijinair9@gmail.com>
2026-07-11 17:40:36 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -5,16 +5,14 @@ from time import sleep
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-@pytest.fixture(params=["chrome",'firefox'])
+@pytest.fixture(params=["chrome"])
 
 def setup_and_teardown(request):
 
     parameter = request.param
     if parameter == "chrome":
         driver = webdriver.Chrome()
-    if parameter == "firefox":
-        driver = webdriver.Firefox()
-        
+            
     driver.implicitly_wait(10)
     driver.get("https://www.ixigo.com/")   #launch ixigo app
     driver.maximize_window()
