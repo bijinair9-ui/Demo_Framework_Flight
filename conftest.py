f5932cce6bd60b50f607d937a5ba201f1e274557
bijinair9-ui <bijinair9@gmail.com>
2026-07-11 17:33:49 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,7 +1,6 @@
 import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from time import sleep
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -13,11 +12,9 @@ def setup_and_teardown(request):
     parameter = request.param
     if parameter == "chrome":
         driver = webdriver.Chrome()
-    if parameter == "edge":
-        driver = webdriver.Edge()
-        options.add_argument("--headless=new")
-        driver = webdriver.Edge(EdgeChromiumDriverManager().install())
-
+    if parameter == "firefox":
+        driver = webdriver.FireFox()
+        
     driver.implicitly_wait(10)
     driver.get("https://www.ixigo.com/")   #launch ixigo app
     driver.maximize_window()
